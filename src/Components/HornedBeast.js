9389bb9601d694c0021f25eaf8d8437be6f79867
hayaa1234 <hayalawansah@gmail.com>
2021-09-06 16:34:19 +0300

    lab02

--- src/Components/HornedBeast.js
+++ src/Components/HornedBeast.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 
 export class HornedBeast extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            vote:0,
+        }
+    }
+    addVote=()=>{
+        this.setState({
+            vote:this.state.vote+1
+        })
+    }
     render() {
         return (
             <>
               <h2>{this.props.name}</h2>
               <img  src={this.props.src} alt={this.name} title={this.name}/>
               <p>{this.props.description}</p>
+              <button onClick={this.addVote}>like</button>
+              <p>{this.state.vote}</p>
             </>
         )
     }
